60bd65e3db1dc9f0727186252236525a99fc5ef0
Hasibul-git <93538796+Hasibul-git@users.noreply.github.com>
2021-12-31 18:30:32 +0800

    Api project

--- app/src/main/java/com/e/smartshop/MainActivity.kt
+++ app/src/main/java/com/e/smartshop/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.e.smartshop
 
+import Activities.ProductActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this,ProductActivity::class.java)
+        startActivity(intent)
     }
 }
